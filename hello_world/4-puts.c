1b17efc50df66dc6f5bfbdf916b7d0060e4e3501
Sakina045961 <huseynovas2487@gmail.com>
2025-02-26 12:34:07 +0000

    aj

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-gcc -c puts(Programming is like building a multilingual puzzle)0
+#include<studio.h>
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
