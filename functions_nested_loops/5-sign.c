24727fbc6e10971fa4a380e08057679de6fd17d6
Sakina045961 <huseynovas2487@gmail.com>
2025-03-02 12:40:50 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Returns and prints
+ * @n: variable
+ * Return: if positive1, zero0, negative -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
