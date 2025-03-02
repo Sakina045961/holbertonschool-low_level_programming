2de77e04a5d567cd79e28f3148efe3265df0c840
Sakina045961 <huseynovas2487@gmail.com>
2025-03-02 12:36:22 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - lowercase
+ * @n: some variables
+ * Return: Always 0 (Success)
+ */
+int _islower(int n)
+{
+	if (n >= 97 && n <= 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
