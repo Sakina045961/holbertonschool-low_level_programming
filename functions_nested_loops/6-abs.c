98c68b4bd115dfe6207b72b3f930502056434c13
Sakina045961 <huseynovas2487@gmail.com>
2025-03-02 12:43:31 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - absolute value of an integer.
+ *@x: variable for function
+ *Return: value for return
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = x * -1;
+	}
+	return (x);
+}
