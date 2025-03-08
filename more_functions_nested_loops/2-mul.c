e2d4fb08893ccb941252664751c38851b9a5a23d
Sakina045961 <huseynovas2487@gmail.com>
2025-03-08 16:55:10 +0000

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
 	int a, b;
 
+	/* Ask user to input two numbers */
 	printf("Write 2 numbers for multiplication\n");
+
+	/* Read the two numbers from user input */
 	scanf("%d %d", &a, &b);
+
+	/* Display the result of the multiplication */
 	printf("Multiplication of %d and %d is %d\n", a, b, (a * b));
+
 	return (0);
 }
