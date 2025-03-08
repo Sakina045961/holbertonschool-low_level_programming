e2fa1e0f4e98b930017c3b7edd9319ccf6eb6889
Sakina045961 <huseynovas2487@gmail.com>
2025-03-08 17:11:09 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int result;
+
+	result = mul(2, 3);
+	printf("%d\n", result);
+
+	return (0);
+}
+
